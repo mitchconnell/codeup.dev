0b68d124f08bb92526d657a051cab5135b12a314
Mitch <mitchconnell@hotmail.com>
2014-02-18 13:54:28 -0600

    new

--- my-first-form.php
+++ my-first-form.php
@@ -1,10 +1,20 @@
+<?php
+
+echo "<p>GET:</p>";
+var_dump($_POST);
+
+
+echo "<p>POST:</p>";
+var_dump($_POST);
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>My First HTML Form</title>
     </head>
     <body>
-        <form method="POST" action="http://requestb.in/14q4vkn1?inspect">
+        <form method="GET" action="">
         <p>
             <label for="username" >Username</label>
             <input id="username" placeholder="Enter your username" name="username" type="text">
@@ -13,20 +23,22 @@
             <label for="password" >Password</label>
             <input id="password" placeholder="Enter your password"name="password" type="password">
         </p>
+
         <label for= "comments">Comments</label>
-        <textarea id= "comments" name= "comments"></textarea>        
+        <textarea id= "comments" name= "comments"></textarea>
+
         <p>
             <button type="submit">Login by clicking here</button>
         </p>   
-
         <p>
-
-        <label for="email_body">Compose an Email</label>
-        <textarea id="email_body" rows="5" cols="40" name="email_body">Content Here</textarea>
+        <p>
+            <label for="email_body">Compose an Email</label>
+        </p>    
+        <p>    
+            <textarea id="email_body" rows="5" cols="40" name="email_body">Content Here</textarea>
         </p>
+        <p>
+            <input type="checkbox" value="yes" checked> Save a copy to your sent folder?</input>           
     </form>
 </body>   
 </html>
-
-
-
