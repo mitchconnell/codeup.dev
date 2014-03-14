2a15821fd0b98870d0ce9a23d87b6d0337caf1dc
Mitch <mitchconnell@hotmail.com>
2014-03-14 09:29:47 -0500

    mysql

---
+++ php_mysqli.php
@@ -0,0 +1,114 @@
+<?php
+
+$mysqli = new mysqli('127.0.0.1', 'mitchell', 'F00die88', 'national_parks');
+// Check for errors
+if ($mysqli->connect_errno) {
+    echo 'Failed to connect to MySQL: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error;
+}
+
+echo $mysqli->host_info . "\n";
+
+// $query = 'CREATE TABLE national_parks (
+//     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+//     name VARCHAR(50) NOT NULL,
+//     location VARCHAR(50) NOT NULL,
+//     description TEXT(240) NOT NULL,
+//     date_established DATE NOT NULL,
+//     area_in_acres FLOAT(10,2) DEFAULT 0,
+//     PRIMARY KEY (id)
+// );';
+// Run query, if there are errors then display them
+//if (!$mysqli->query($query)) {
+    //throw new Exception("Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error);
+//}
+
+// $query = "INSERT INTO users (email, name) VALUES ('jason@codeup.com', 'Jason Straughan');";
+
+// // Execute Query
+// if (!$mysqli->query($query)) {
+//     throw new Exception("Query failed: (" . $mysqli->errno . ") " . $mysqli->error);
+// }
+$parks = [
+	[
+		'name' => 'Acadia',
+		'location' => 'Maine',
+		'description' => 'Covering most of Mount Desert Island and other coastal islands, Acadia features the tallest mountain on the Atlantic coast of the United States, granite peaks, ocean shoreline, woodlands, and lakes. There are freshwater, estuary, forest, and intertidal habitats.',
+		'date_established' => '1919-02-26',
+		'area_in_acres' => '47,389.67 acres',
+	],
+	[
+		'name' => 'American Samoa',
+		'location' => 'American Samoa',
+		'description' => 'The southernmost national park is on three Samoan islands and protects coral reefs, rainforests, volcanic mountains, and white beaches. The area is also home to flying foxes, brown boobies, sea turtles, and 900 species of fish.',
+		'date_established' => ', 1988-10-31',
+		'area_in_acres' => '9,000.00 acres',
+	],
+	[
+		'name' => 'Arches',
+		'location' => 'Utah',
+		'description' => 'This site features more than 2,000 natural sandstone arches, including the Delicate Arch. In a desert climate millions of years of erosion have led to these structures, and the arid ground has life-sustaining soil crust and potholes, natural water-collecting basins. Other geologic formations are stone columns, spires, fins, and towers.',
+		'date_established' => ', 1971-11-12',
+		'area_in_acres' => '76,518.98 acres',
+	],
+	[
+		'name' => 'Badlands',
+		'location' => 'South Dakota',
+		'description' => 'The Badlands are a collection of buttes, pinnacles, spires, and grass prairies. It has the worlds richest fossil beds from the Oligocene epoch, and there is wildlife including bison, bighorn sheep, black-footed ferrets, and swift foxes.',
+		'date_established' => '1978-11-10',
+		'area_in_acres' => '242,755.94 acres',
+	],
+	[
+		'name' => 'Big Bend',
+		'location' => 'Texas',
+		'description' => 'Named for the Bend of the Rio Grande along the US–Mexico border, this park includes a part of the Chihuahuan Desert. A wide variety of Cretaceous and Tertiary fossils as well as cultural artifacts of Native Americans exist within its borders.',
+		'date_established' => '1944-6-12',
+		'area_in_acres' => '801,163.21 acres',
+	],
+	[
+		'name' => 'Biscayne',
+		'location' => 'Florida',
+		'description' => 'Located in Biscayne Bay, this park at the north end of the Florida Keys has four interrelated marine ecosystems: mangrove forest, the Bay, the Keys, and coral reefs. Threatened animals include the West Indian Manatee, American crocodile, various sea turtles, and peregrine falcon.',
+		'date_established' => '1980-6-28',
+		'area_in_acres' => '172,924.07 acres',
+	],
+	[
+		'name' => 'Black Canyon',
+		'location' => 'Colorado',
+		'description' => 'The park protects a quarter of the Gunnison River, which has dark canyon walls from the Precambrian era. The canyon has very steep descents, and it is a site for river rafting and rock climbing. The narrow, steep canyon, made of gneiss and schist, is often in shadow, appearing black.',
+		'date_established' => '1999-10-21',
+		'area_in_acres' => '32,950.03 acres',
+	],
+	[
+		'name' => 'Bryce Canyon',
+		'location' => 'Utah',
+		'description' => 'Bryce Canyon is a giant natural amphitheatre along the Paunsaugunt Plateau. The unique area has hundreds of tall sandstone hoodoos formed by erosion. The region was originally settled by Native Americans and later by Mormon pioneers.',
+		'date_established' => '1928-2-25',
+		'area_in_acres' => '35,835.08 acres',
+	],
+	[
+		'name' => 'Canyonlands',
+		'location' => 'Utah',
+		'description' => 'This landscape was eroded into canyons, buttes, and mesas by the Colorado River, Green River, and their tributaries, which divide the park into three districts. There are rock pinnacles and other naturally sculpted rock, as well as artifacts from Ancient Pueblo Peoples.',
+		'date_established' => '1964-9-12',
+		'area_in_acres' => '337,597.83 acres',
+	],
+	[
+		'name' => 'Capitol Reef',
+		'location' => 'Utah',
+		'description' => 'This landscape was eroded into canyons, buttes, and mesas by the Colorado River, Green River, and their tributaries, which divide the park into three districts. There are rock pinnacles and other naturally sculpted rock, as well as artifacts from Ancient Pueblo Peoples.',
+		'date_established' => '1971-12-18',
+		'area_in_acres' => '337,597.83 acres',
+	],	
+];
+
+foreach ($parks as $park) {
+	$description = $mysqli->real_escape_string($park['description']); 
+    $query = "INSERT INTO parks (name, location, description, date_established, area_in_acres) VALUES (";
+    $query .= "'" . $park['name'] . "', ";
+    $query .= "'" . $park['location'] . "', ";
+    $query .= "'" . $description . "', ";
+    $query .= "'" . $park['date_established'] . "', ";
+    $query .= "'" . $park['area_in_acres'] . "' ";
+   	$query .= ");";
+    $mysqli->query($query);
+}
